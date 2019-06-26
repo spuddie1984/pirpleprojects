3f3715a3f28a796c4fca0eed289a5b1a4516e208
spuddie1984 <spuddie1984@hotmail.com>
2019-06-26 22:35:37 +0800

    Initial Python is Easy Basic Loops Assignment Commit

---
+++ python-is-easy/basic loops/fizzbuzz.py
@@ -0,0 +1,10 @@
+"""
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz".
+"""
+
+"""
+Extra Credit:
+Instead of only printing "fizz", "buzz", and "fizzbuzz", add a fourth print statement: "prime". You should print this whenever you encounter a number that is prime (divisible only by itself and one). As you implement this, don't worry about the efficiency of the algorithm you use to check for primes. It's okay for it to be slow.
+"""
