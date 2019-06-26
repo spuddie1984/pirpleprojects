90368a054d0360f925ea9155e58a9d80da4fba4c
spuddie1984 <spuddie1984@hotmail.com>
2019-06-26 15:00:36 +0800

    Initial commit for the Lists Assignment

---
+++ python-is-easy/lists/list.py
@@ -0,0 +1,14 @@
+"""
+Create a global variable called myUniqueList
+
+create a function that allows you to add things to that list. Anything that's passed to this function should get added to myUniqueList, unless its value already exists in myUniqueList. If the value doesn't exist already, it should be added and the function should return True. If the value does exist, it should not be added, and the function should return False.
+
+add some code below your function that tests it out
+it should print the value of myUniqueList to show that it worked.
+"""
+
+
+"""
+Extra Credit:
+Add another function that pushes all the rejected inputs into a separate global array called myLeftovers. If someone tries to add a value to myUniqueList but it's rejected (for non-uniqueness), it should get added to myLeftovers instead.
+"""
