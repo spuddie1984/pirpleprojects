29c027b01d3b086dd15d9488c73b0ae28f424433
spuddie1984 <spuddie1984@hotmail.com>
2019-06-27 15:44:21 +0800

    Initial commit for Ptyhon is Easy Advanced Loops

---
+++ python-is-easy/advanced loops/draw.py
@@ -0,0 +1,5 @@
+"""
+Create a function that takes in two parameters: rows, and columns, both of which are integers. 
+The function should then proceed to draw a playing board, the same number of rows and columns as specified. 
+After drawing the board, your function should return True.
+"""
