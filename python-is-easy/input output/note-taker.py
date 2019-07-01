ccdd1ea9cfaeed00b6c16d57f682dd175b017419
spuddie1984 <spuddie1984@hotmail.com>
2019-07-01 22:02:48 +0800

    Python is Easy Input Output Assignment

--- python-is-easy/input output/note-taker.py
+++ python-is-easy/input output/note-taker.py
@@ -10,14 +10,7 @@ If the user wants to read the file it should simply show the contents of the fil
 If the user wants to start over then the file should be deleted and another empty one made in its place. 
 If a user elects to append the file, then they should be able to enter more text, and that text should be added 
 to the existing text in the file. 
-"""
-# need to import a library for checking if a folder/file exists
-import os
 
-# Ask the user for a filename if it exists
-fileName = input("Please enter a filename to use\n")
-
-"""
 Extra Credit:
 Allow the user to select a 4th option:
 D) Replace a single line
@@ -26,4 +19,52 @@ they will then need to be prompted for 2 bits of information:
 1) The line number they want to update.
 2) The text that should replace that line.
 """
+# need to import a library for checking if a folder/file exists
+import os
 
+# Ask the user for a filename if it exists and the complete absolute path to the file
+fileName = input("Please enter a filename to use(without the extension)\n")
+fileName += ".txt"
+# we need to append the absolute path to the file name or it will be stored in the root directory
+completeFileName = os.path.join(os.path.dirname(__file__),fileName)
+
+# check to see if the file exists
+if os.path.isfile(completeFileName):
+    # ask what to do with the existing file and use if elif statements accordingly
+    fileMode = input("Would you like to\n1. Read the File(enter r)\n2. Delete the file and start over(enter w)\n3. Append the File(enter a)\n4. Replace a Single line(enter single line)\n")
+    
+    # used so that we can store the "single line" string (one of the options) for an if statement
+    lineAdder = ""
+    # set the file mode to read
+    if fileMode == "single line":
+        lineAdder = fileMode
+        fileMode = "r"      
+    fileAction = open(completeFileName, fileMode)
+    # if/elif logic according to what the user chooses
+    if lineAdder == "single line":
+        lineNo = int(input("Which line would you like to replace?\n"))
+        newText = input("What is the new text?\n")
+        # readlines returns an array of lines from the text file
+        lineOfText = fileAction.readlines()
+        # replace the selected line with the new users text
+        lineOfText[lineNo -1] = newText + "\n" 
+        print(lineOfText) # For testing !!!
+        # we have to open the file in write mode 
+        fileAction = open(completeFileName, "w")    
+        fileAction.writelines(lineOfText)
+    elif fileMode == "r":
+        print(fileAction.read())
+    elif fileMode == "w":
+        reWrite = input("You have choosen to rewrite the file please enter some new text\n")
+        fileAction.write(reWrite)
+    elif fileMode == "a":
+        append = input("you have choosen to append to your previously made file please add some text\n")
+        fileAction.write("\n" + append)
+    # dont forget to close the file 
+    fileAction.close()
+# file doesnt exist lets create a new one with the name that was enter for the mystery file
+else:
+    newFileText = input("Please enter some text for your new file then press enter\n")
+    newFile = open(completeFileName, "x")
+    newFile.write(newFileText)
+    newFile.close()
