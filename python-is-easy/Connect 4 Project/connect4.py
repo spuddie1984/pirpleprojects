65d596d65f695719331a6c7b862508d3e6ada283
spuddie1984 <spuddie1984@hotmail.com>
2019-07-01 22:07:12 +0800

    Initial commit Python is  Easy Connect 4 Project

---
+++ python-is-easy/Connect 4 Project/connect4.py
@@ -0,0 +1,15 @@
+"""
+Once you've got the rules down, your assignment should be fairly straightforward. 
+You'll want to draw the board, and allow two players to take turns placing their pieces on the board 
+(but as you learned above, they can only do so by choosing a column, not a row). 
+The first player to get 4 across or diagonal should win!
+
+Extra Credit:
+
+Want to try colorful pieces instead of X and O? 
+First you'll need to figure out how to import a package like termcolor into your project. 
+We're going to cover importing later in the course, 
+but try and see if you can figure it out on your own. 
+Or you might be able to find unicode characters to use instead, 
+depending on what your system supports. Here's a hint: print(u'\u2B24')
+"""
